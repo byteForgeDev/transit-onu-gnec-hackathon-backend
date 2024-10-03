934fba7bf1d86a5fe60d81f2ee8cff2b57d0f8ab
mizamarzes <mizamarzes@gmail.com>
2024-10-03 17:33:48 -0500

    first commit

---
+++ src/main/java/com/byteforgedev/transit_onu_gnec_hackaton_backend/TransitOnuGnecHackatonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.byteforgedev.transit_onu_gnec_hackaton_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitOnuGnecHackatonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitOnuGnecHackatonBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/byteforgedev/transit_onu_gnec_hackaton_backend/TransitOnuGnecHackatonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byteforgedev.transit_onu_gnec_hackaton_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitOnuGnecHackatonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
