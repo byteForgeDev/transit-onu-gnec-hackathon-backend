9cf9822acbd2a170a570c151921665f169455c8a
oscarArismendi <oscarfernandoarismendigalvis@gmail.com>
2024-10-25 21:06:29 -0500

    feat: add initial roles in the database

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO roles (rol, created_at) VALUES ('ADMIN', NOW());
+INSERT INTO roles (rol, created_at) VALUES ('USER', NOW());
+INSERT INTO roles (rol, created_at) VALUES ('COMPANY MANAGER', NOW());
+INSERT INTO roles (rol, created_at) VALUES ('DRIVER', NOW());
+INSERT INTO roles (rol, created_at) VALUES ('MODERATOR', NOW());
