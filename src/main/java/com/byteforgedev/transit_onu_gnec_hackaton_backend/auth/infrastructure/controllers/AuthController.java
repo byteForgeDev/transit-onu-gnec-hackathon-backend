3045edaf3de4994e6f91157ec86a6bc68c89d817
oscarArismendi <oscarfernandoarismendigalvis@gmail.com>
2024-10-25 21:03:25 -0500

    chore: add login exampleto swagger

--- src/main/java/com/byteforgedev/transit_onu_gnec_hackaton_backend/auth/infrastructure/controllers/AuthController.java
+++ src/main/java/com/byteforgedev/transit_onu_gnec_hackaton_backend/auth/infrastructure/controllers/AuthController.java
@@ -11,6 +11,9 @@ import com.byteforgedev.transit_onu_gnec_hackaton_backend.auth.domain.dto.AuthRe
 import com.byteforgedev.transit_onu_gnec_hackaton_backend.auth.domain.dto.LoginRequest;
 import com.byteforgedev.transit_onu_gnec_hackaton_backend.auth.domain.dto.RegisterRequest;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -21,11 +24,25 @@ public class AuthController {
 
     private final AuthServiceImpl authAdapter;
 
+     
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request){
         return ResponseEntity.ok(authAdapter.login(request));
     }
 
+    @Operation(
+            summary = "Login User",
+            description = "Authenticate a user and return the authentication token along with user details.",
+
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Authentication request with username and password",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(value = "{  \"username\": \"Sample User\", \"password\": \"123Abc#\",\"email\": \"sample@example.com\",\"photoProfileUrl\": \"\",\"roles\": [\"ADMIN\"] }")
+                    )
+            )
+    )
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request){
         return ResponseEntity.ok(authAdapter.register(request));
